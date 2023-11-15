10dc12c39fc75bf75605ccdaf17e7562f76c9cb6
Gustavo Daniel Unser <gustavo.unser@segfy.com>
2023-11-15 00:07:45 -0300

    Feat(Controllers): Item controller update

--- app/controllers/api/V1/items_controller.rb
+++ app/controllers/api/V1/items_controller.rb
@@ -1,6 +1,26 @@
 module Api
 	module V1
 		class ItemsController < ApplicationController
+            def create
+                item = Item.find_or_initialize_by(name: params['name'], description: params['description'])
+                category = Category.find_by(name: params['category'])
+
+                if category.blank?
+                    render json: {status: 'ERROR', message: 'Category not found', data:{}}, status: :unprocessable_entity
+                else
+                    action = category.new_record? ? "created" : "updated"
+
+                    item.category = category
+                    item.quantity = params['quantity']
+
+                    if item.save
+                        render json: {status: 'SUCCESS', message: "Item #{action} successfully", data:item}, status: :ok
+                    else
+                        render json: {status: 'ERROR', message: 'Could not create Item', data:item.errors}, status: :unprocessable_entity
+                    end
+                end
+            end
+
             def index
                 items = Item.all
                 render json: {status: 'SUCCESS', message: 'Items list all', data:items}, status: :ok
